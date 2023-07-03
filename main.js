aa30d8839546d06dfd857bffd21b431a7122563c
nyrah-kathuria <89901792+nyrah-kathuria@users.noreply.github.com>
2023-07-03 18:56:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX=0;
+noseY=0;
+leftWristX=0;
+rightWristX=0;
+difference=0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -15,12 +21,29 @@ function modelLoaded() {
 
 function draw(){
     background('#969A97');
+    document.getElementById("square_side").innerHTML = "Width and Height of a Square will be =" + difference +"px";
+    fill('F90093');
+    stroke('F90093');
+    textSize(difference);
+    text("Hello",noseX, noseY);
 }
+
+
 
 function gotPoses(results) 
 {
     if(results.length> 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX =" + noseX + "noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX =" + "difference =" + difference);
+
     }
 }
